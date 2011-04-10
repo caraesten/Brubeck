fd9ef13f59c50e3e4f106b1872f5fe0d4db37275
Esten Hurtle <dondeesten@gmail.com>
2011-04-10 16:06:11 -0500

    added admins for management, publishing

---
+++ management/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from brubeck.management.models import WebFront
+
+admin.site.register(WebFront)
---
+++ publishing/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from brubeck.publishing.models import Publication,Section,Volume,Issue
+
+admin.site.register(Publication)
+admin.site.register(Section)
+admin.site.register(Volume)
+admin.site.register(Issue)
