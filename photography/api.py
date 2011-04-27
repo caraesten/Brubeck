277dbdec659b5a5287e1fbffc5674c9df09dc025
Esten Hurtle <esten.hurtle@gmail.com>
2011-04-27 14:13:52 -0500

    Photo API fixes

--- photography/api.py
+++ photography/api.py
@@ -6,8 +6,8 @@ class PhotoResource(ModelResource):
 		issue = fields.ForeignKey(IssueResource, 'issue')
 		section = fields.ForeignKey(SectionResource, 'section')
 		tag = fields.ToManyField('brubeck.tagging.api.TagResource', 'tag')
-        class Meta:
-                queryset = Photo.objects.all()
-                resource_name = 'photo'
-                allowed_methods = ['get']
+		class Meta:
+			queryset = Photo.objects.all()
+			resource_name = 'photo'
+			allowed_methods = ['get']
 
