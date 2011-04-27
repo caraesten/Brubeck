f185d027d27650bd638639f6b27e93c35f4e7e54
Esten Hurtle <esten.hurtle@gmail.com>
2011-04-27 14:26:38 -0500

    Multimedia API fixes

--- multimedia/api.py
+++ multimedia/api.py
@@ -1,23 +1,24 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
 from brubeck.multimedia.models import AudioClip,Slideshow,Video
 
 class AudioResource(ModelResource):
-	    tag = fields.ToManyField('brubeck.tagging.api.TagResource', 'tag')
-        class Meta:
-                queryset = AudioClip.objects.all()
-                resource_name = 'audioclip'
-                allowed_methods = ['get']
+		tag = fields.ToManyField('brubeck.tagging.api.TagResource', 'tag')
+		class Meta:
+			queryset = AudioClip.objects.all()
+			resource_name = 'audioclip'
+			allowed_methods = ['get']
 
 class SlideshowResource(ModelResource):
-	    tag = fields.ToManyField('brubeck.tagging.api.TagResource', 'tag')
-        class Meta:
-                queryset = Slideshow.objects.all()
-                resource_name = 'slideshow'
-                allowed_methods = ['get']
+		tag = fields.ToManyField('brubeck.tagging.api.TagResource', 'tag')
+		class Meta:
+			queryset = Slideshow.objects.all()
+			resource_name = 'slideshow'
+			allowed_methods = ['get']
 
 class VideoResource(ModelResource):
 		tag = fields.ToManyField('brubeck.tagging.api.TagResource', 'tag')
-        class Meta:
-                queryset = Video.objects.all()
-                resource_name = 'video'
-                allowed_methods = ['get']
+		class Meta:
+			queryset = Video.objects.all()
+			resource_name = 'video'
+			allowed_methods = ['get']
