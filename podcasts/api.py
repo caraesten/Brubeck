62ca425ae43d18d6e2689276da523b45422740fc
Esten Hurtle <esten.hurtle@gmail.com>
2011-04-27 14:24:29 -0500

    Podcast API fixes

--- podcasts/api.py
+++ podcasts/api.py
@@ -5,15 +5,14 @@ from brubeck.podcasts.models import Episode,Channel
 class ChannelResource(ModelResource):
 		class Meta:
 			queryset = Channel.objects.all()
-            resource_name = 'channel'
-            allowed_methods = ['get']
+			resource_name = 'channel'
+			allowed_methods = ['get']
 
 
 
 class PodcastResource(ModelResource):
 		channel = fields.ForeignKey(ChannelResource, 'channel')
 		class Meta:
-            queryset = Episode.objects.all()
-            resource_name = 'podcast'
-            allowed_methods = ['get']
-
+			queryset = Episode.objects.all()
+			resource_name = 'podcast'
+			allowed_methods = ['get']
