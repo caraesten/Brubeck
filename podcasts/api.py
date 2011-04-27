b55da8c03b7053b54d015a4323262b1f3c4b6e01
Esten Hurtle <esten.hurtle@gmail.com>
2011-04-27 14:22:34 -0500

    Podcast API fixes

--- podcasts/api.py
+++ podcasts/api.py
@@ -1,18 +1,19 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
 from brubeck.podcasts.models import Episode,Channel
 
 class ChannelResource(ModelResource):
-        class Meta:
-                queryset = Channel.objects.all()
-                resource_name = 'channel'
-                allowed_methods = ['get']
+		class Meta:
+			queryset = Channel.objects.all()
+            resource_name = 'channel'
+            allowed_methods = ['get']
 
 
 
 class PodcastResource(ModelResource):
 		channel = fields.ForeignKey(ChannelResource, 'channel')
-        class Meta:
-                queryset = Episode.objects.all()
-                resource_name = 'podcast'
-                allowed_methods = ['get']
+		class Meta:
+            queryset = Episode.objects.all()
+            resource_name = 'podcast'
+            allowed_methods = ['get']
 
